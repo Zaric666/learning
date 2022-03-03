513fbda261a7520e787de225911f7c31aa927619
zhangli <zhangli@mia.com>
2022-03-03 17:49:32 +0800

    grpc etcd service discovery

--- grpc/etcd/client/main.go
+++ grpc/etcd/client/main.go
@@ -3,20 +3,34 @@ package main
 import (
 	"context"
 	"fmt"
+	discovery "github.com/Zaric666/learning/grpc/etcd/dicovery"
 	pb "github.com/Zaric666/learning/grpc/proto/echo"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/balancer/roundrobin"
+	"google.golang.org/grpc/resolver"
 	"log"
 )
 
+const App = "base-hello"
+
 func main() {
-	conn, err := grpc.Dial("127.0.0.1:8083", grpc.WithInsecure())
+	options := []grpc.DialOption{
+		grpc.WithInsecure(),
+		grpc.WithBalancerName(roundrobin.Name),
+	}
+
+	addrs := []string{"127.0.0.1:12379"}
+	r := discovery.NewResolver(addrs, zap.NewNop())
+	resolver.Register(r)
+
+	conn, err := grpc.Dial("etcd:///"+App, options...)
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 
 	client := pb.NewEchoClient(conn)
-
 	res, err := client.UnaryEcho(context.Background(), &pb.EchoRequest{
 		Message: "Hi, I'm client A",
 	})
